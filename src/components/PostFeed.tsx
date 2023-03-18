7d413e087e0dd6a19cce74cbebcf50fad9706482
Megan Cindric <megancindric@gmail.com>
2023-03-18 14:52:12 -0500

    🐛 FIX: Added mb to allow for full post display

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -3,7 +3,7 @@ import { Post, PostList } from '../postInterfaces';
 import SinglePost from './SinglePost';
 
 const PostFeed: React.FC<PostList> = (props) => {
-    return ( <div className=' flex flex-col items-center gap-6 w-1/2'>
+    return ( <div className=' flex flex-col items-center gap-6 w-1/2 mb-10'>
         {props.postList.map((post:Post, index) => (
             <SinglePost key={index} name={post.name} body={post.body}/>
         )).reverse()}
